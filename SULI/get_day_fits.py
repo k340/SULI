455b19267a48f466154533f4d37aa4a1709ef71d
giacomov <giacomo.vianello@gmail.com>
2016-07-06 16:37:49 -0700

    Added a bunch of parameters and completed the gtselect command

--- SULI/get_day_fits.py
+++ SULI/get_day_fits.py
@@ -8,6 +8,7 @@
 import argparse
 from GtApp import GtApp
 from astropy.io import fits
+import numpy as np
 
 # execute only if run from command line
 if __name__=="__main__":
@@ -18,34 +19,62 @@ if __name__=="__main__":
     # add the arguments needed to the parser
     parser.add_argument("--in_file", help="Fits file containing data to be segmented", required=True,
                         type=str)
+    parser.add_argument("--evclass", help="Event class to use for cutting the data (default: 128)", required=True,
+                        type=int)
+    parser.add_argument("--zmax", help="Zenith cut for the events", required=True,
+                        type=float)
 
     # parse the arguments
     args = parser.parse_args()
 
     # get input data file from parser and retrieve start and stop times
-    data = fits.open(args.in_file)
 
-    begin = data[0].header['TSTART']
-    end = data[0].header['TSTOP']
+    ft1 = fits.open(args.in_file)
+
+    with fits.open(args.in_file) as ft1:
+
+        event_file_start = ft1[0].header['TSTART']
+        event_file_end = ft1[0].header['TSTOP']
+
+    duration = event_file_end - event_file_start
+
+    n_days = duration / 86400.0
+
+    print("Found %s days in file" % n_days)
+
+    n_days_rounded = int(np.ceil(n_days))
+
+    print("Rounded up to %s" % n_days_rounded)
 
     # iterate over input file, creating new fits file for every 86400 seconds of data (24 hours)
 
-    while begin < end:
-        cut_time = begin + 86400
+    for i in range(n_days_rounded):
+
+        this_start = event_file_start + i * 86400.0
+
+        this_stop = event_file_start + (i+1) * 86400.0
 
         gtselect = GtApp('gtselect')
 
         gtselect['infile'] = args.in_file
 
-        gtselect['outfile'] = args.in_file.rsplit(".", 1)[0] + '_' + str(begin) + '_' + str(cut_time) + '.fits'
+        gtselect['outfile'] = args.in_file.rsplit(".", 1)[0] + '_' + str(this_start) + '_' + str(this_stop) + '.fits'
 
-        gtselect['tmin'] = begin
+        gtselect['tmin'] = this_start
 
-        gtselect['tmax'] = cut_time
+        gtselect['tmax'] = this_stop
+
+        gtselect['evclass'] = args.evclass
+
+        gtselect['zmax'] = args.zmax
+
+        # Avoid cutting using an ROI
+
+        gtselect['rad'] = 180.0
+
+        # Avoid cutting in energy
+        gtselect['emin'] = 10.0 # MeV
+        gtselect['emax'] = 500000.0 # MeV
 
         gtselect.run()
-
-        begin += 86400
-
-    data.close()
 
