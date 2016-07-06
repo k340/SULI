11f50347d1054f7681a8e212d04791d80469b860
giacomov <giacomo.vianello@gmail.com>
2016-07-05 17:00:11 -0700

    First setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+setup(
+    name="SULI",
+
+    packages=['SULI'],
+
+    version='0.0.1',
+
+    description="SULI project",
+
+    author='Kelin Kurzer-Ogul',
+
+    author_email='kelin.kurzerogul@gmail.com',
+
+    url='https://github.com/k340/SULI',
+
+    keywords=['SULI', 'Fermi', 'LAT', 'transients'],
+
+    classifiers=[],
+
+    install_requires=['numpy'],
+
+)
+
