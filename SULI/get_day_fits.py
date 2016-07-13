0270e763ba6fe3d9c81c4de7c90b2bc0e57473eb
k340 <kelin.kurzerogul@gmail.com>
2016-07-13 12:53:26 -0400

    updated get_day_fits (0.4)

--- SULI/get_day_fits.py
+++ SULI/get_day_fits.py
@@ -9,16 +9,21 @@ import argparse
 from GtApp import GtApp
 from astropy.io import fits
 import numpy as np
+from SULI.execute_command import execute_command
 
 # execute only if run from command line
-if __name__=="__main__":
+if __name__ == "__main__":
 
     # create parser for this script
     parser = argparse.ArgumentParser('Split Fermi data file (.fits) into multiple smaller files each spanning 24 hours')
 
     # add the arguments needed to the parser
-    parser.add_argument("--in_file", help="Fits file containing data to be segmented", required=True,
+    parser.add_argument("--in_ft1", help="Ft1 file containing data to be segmented", required=True,
                         type=str)
+    parser.add_argument("--in_ft2", help="Ft2 file containing data to be segmented", required=True,
+                        type=str)
+    parser.add_argument("--buffer", help="Ft2 file is expanded backwards and forwards in time by this amount to ensure"
+                                         "it covers a time interval >= Ft1", required=True)
     parser.add_argument("--evclass", help="Event class to use for cutting the data (default: 128)", required=True,
                         type=int)
     parser.add_argument("--zmax", help="Zenith cut for the events", required=True,
@@ -27,11 +32,9 @@ if __name__=="__main__":
     # parse the arguments
     args = parser.parse_args()
 
-    # get input data file from parser and retrieve start and stop times
+    # get input ft1 file from parser and retrieve start and stop times
 
-    ft1 = fits.open(args.in_file)
-
-    with fits.open(args.in_file) as ft1:
+    with fits.open(args.in_ft1) as ft1:
 
         event_file_start = ft1[0].header['TSTART']
         event_file_end = ft1[0].header['TSTOP']
@@ -46,7 +49,7 @@ if __name__=="__main__":
 
     # print("Rounded up to %s" % n_days_rounded)
 
-    # iterate over input file, creating new fits file for every 86400 seconds of data (24 hours)
+    # iterate over input ft1, creating new fits file for every 86400 seconds of data (24 hours)
 
     for i in range(n_days_rounded):
 
@@ -56,9 +59,9 @@ if __name__=="__main__":
 
         gtselect = GtApp('gtselect')
 
-        gtselect['infile'] = args.in_file
+        gtselect['infile'] = args.in_ft1
 
-        gtselect['outfile'] = args.in_file.rsplit(".", 1)[0] + '_' + str(this_start) + '.fits'
+        gtselect['outfile'] = args.in_ft1.rsplit(".", 1)[0] + '_' + str(this_start) + 'ft1.fits'
 
         gtselect['tmin'] = this_start
 
@@ -73,13 +76,27 @@ if __name__=="__main__":
         gtselect['rad'] = 180.0
 
         # Avoid cutting in energy
-        gtselect['emin'] = 10.0 # MeV
-        gtselect['emax'] = 500000.0 # MeV
+        gtselect['emin'] = 10.0  # MeV
+        gtselect['emax'] = 500000.0  # MeV
 
         gtselect.run()
 
+    # pull relevant data interval from ft2 input
 
-'''First make this take an ft1 and ft2, gtsel on ft1 and do the other ftcopy thing on ft2 master to get ft2. dont forget time buffer
+    with fits.open(args.in_ft2) as ft2:
+
+        # add time buffers to cut
+
+        event_file_start -= args.buffer
+        event_file_end += args.buffer
+
+        cmd_line = "ftcopy '%s[SC_DATA][START > %s && STOP < %s]' output_ft2.fit copyall=true'" % (ft2,
+                                                                                                   event_file_start,
+                                                                                                   event_file_end)
+    execute_command(cmd_line)
+
+
+'''First make this take an ft1 and ft2, gtsel on ft1 and do the other ftcopy thing on ft2 master to get ft2.
     then test whole thing on comp with data already have.
     then test on farm with random day. remember to log on:
         ssh -X suli_students@galprop-cluster
