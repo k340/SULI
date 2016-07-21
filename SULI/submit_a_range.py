d884df46488a4fe04a81dabbfb4ac8f34d49cc10
giacomov <giacomo.vianello@gmail.com>
2016-07-20 17:38:22 -0700

    Crude script to submit a year worth of simulation to the farm

---
+++ SULI/submit_a_range.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import numpy as np
+import subprocess
+
+if __name__=="__main__":
+
+    tstart = 239557420.0
+
+    cmd_line = "qsub -l vmem=10gb  -V -F '--tstart %s " \
+               "--in_ft2 /home/suli_students/suli_kelin/simulation/ft2_1_year.fits " \
+               "--src_dir /home/suli_students/suli_kelin/simulation_input/3FGLSkyPass8R2 " \
+               "--out_dir /home/suli_students/suli_kelin/simulation/generated_data' " \
+               "/home/suli_students/suli_kelin/LAT_transient_search/SULI/SULI/simulate_in_the_farm.py"
+
+    # A year
+
+    tstarts = np.arange(tstart, tstart + (365.0*86400.0), 86400.0)
+
+    for tstart in tstarts:
+
+        this_cmd_line = cmd_line % tstart
+
+        print(this_cmd_line)
+
+        subprocess.check_call(cmd_line)
